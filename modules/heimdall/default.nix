4fc3c27a528a3d0662972029331575ea32331f56
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-28 17:49:44 -0300

    adapt heimdall module with nullable parameters

--- modules/heimdall/default.nix
+++ modules/heimdall/default.nix
@@ -15,44 +15,38 @@ let
 
       amqp_url = lib.mkOption {
         type = lib.types.str;
-        default = null;
+        default = "amqp://guest:guest@localhost:5672/";
         description = "Set AMQP endpoint.";
       };
 
       bor_rpc_url = lib.mkOption {
         type = lib.types.str;
-        default = null;
+        default = "http://localhost:8545";
         description = "Set RPC endpoint for the Bor chain.";
       };
 
       checkpoint_poll_interval = lib.mkOption {
         type = lib.types.str;
-        default = null;
+        default = "30s";
         description = "Set checkpoint poll interval.";
       };
 
       clerk_poll_interval = lib.mkOption {
         type = lib.types.str;
-        default = null;
+        default = "30s";
         description = "Set clerk poll interval.";
       };
 
       eth_rpc_url = lib.mkOption {
         type = lib.types.str;
-        default = null;
+        default = "http://localhost:8545";
         description = "Set RPC endpoint for Ethereum chain.";
       };
 
       heimdall_config = lib.mkOption {
-        type = lib.types.path;
+        type = lib.types.nullOr lib.types.path;
         default = null;
         description = "Override Heimdall config file.";
-      };
-
-      heimdall_rest_server = lib.mkOption {
-        type = lib.types.str;
-        default = null;
-        description = "Set Heimdall REST server endpoint.";
       };
 
       logs_writer_file = lib.mkOption {
@@ -62,13 +56,13 @@ let
       };
 
       main_chain_gas_limit = lib.mkOption {
-        type = lib.types.int;
+        type = lib.types.nullOr lib.types.int;
         default = null;
         description = "Set main chain gas limit.";
       };
 
       main_chain_max_gas_price = lib.mkOption {
-        type = lib.types.int;
+        type = lib.types.nullOr lib.types.int;
         default = null;
         description = "Set main chain max gas price.";
       };
@@ -80,33 +74,15 @@ let
       };
 
       no_ack_wait_time = lib.mkOption {
-        type = lib.types.str;
+        type = lib.types.nullOr lib.types.str;
         default = null;
         description = "Set time ack service waits to clear buffer.";
       };
 
-      noack_poll_interval = lib.mkOption {
-        type = lib.types.str;
+      seeds = lib.mkOption {
+        type = lib.types.nullOr lib.types.str;
         default = null;
-        description = "Set no acknowledge poll interval.";
-      };
-
-      span_poll_interval = lib.mkOption {
-        type = lib.types.str;
-        default = null;
-        description = "Set span poll interval.";
-      };
-
-      syncer_poll_interval = lib.mkOption {
-        type = lib.types.str;
-        default = null;
-        description = "Set syncer poll interval.";
-      };
-
-      tendermint_rpc_url = lib.mkOption {
-        type = lib.types.str;
-        default = null;
-        description = "Set RPC endpoint for Tendermint.";
+        description = "Override seeds.";
       };
 
       trace = lib.mkOption {
@@ -115,10 +91,10 @@ let
         description = "Print out full stack trace on errors.";
       };
 
-      seeds = lib.mkOption {
-        type = lib.types.str;
-        default = null;
-        description = "Override seeds.";
+      extraArgs = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [];
+        description = "Additional arguments for the Heimdall executable.";
       };
 
       package = lib.mkPackageOption pkgs [ "heimdall" ] { };
@@ -131,12 +107,13 @@ in {
     services.heimdall-polygon = lib.mkOption {
       type = lib.types.attrsOf (lib.types.submodule heimdallOpts);
       default = {};
-      description = "Configuration for one or more Heimdall node instances.";
+      description = "Specification of one or more Heimdall node instances.";
     };
   };
 
   ###### Implementation
   config = lib.mkIf (eachHeimdall != {}) {
+
     environment.systemPackages = lib.flatten (lib.mapAttrsToList (name: cfg: [
       cfg.package
     ]) eachHeimdall);
@@ -157,24 +134,20 @@ in {
               start \
               --home ${dataDir} \
               --chain ${cfg.chain} \
-              ${lib.optionalString (cfg.amqp_url != null) "--amqp_url ${cfg.amqp_url}"} \
-              ${lib.optionalString (cfg.bor_rpc_url != null) "--bor_rpc_url ${cfg.bor_rpc_url}"} \
-              ${lib.optionalString (cfg.checkpoint_poll_interval != null) "--checkpoint_poll_interval ${cfg.checkpoint_poll_interval}"} \
-              ${lib.optionalString (cfg.clerk_poll_interval != null) "--clerk_poll_interval ${cfg.clerk_poll_interval}"} \
-              ${lib.optionalString (cfg.eth_rpc_url != null) "--eth_rpc_url ${cfg.eth_rpc_url}"} \
+              --amqp_url ${cfg.amqp_url} \
+              --bor_rpc_url ${cfg.bor_rpc_url} \
+              --checkpoint_poll_interval ${cfg.checkpoint_poll_interval} \
+              --clerk_poll_interval ${cfg.clerk_poll_interval} \
+              --eth_rpc_url ${cfg.eth_rpc_url} \
               ${lib.optionalString (cfg.heimdall_config != null) "--heimdall-config ${cfg.heimdall_config}"} \
-              ${lib.optionalString (cfg.heimdall_rest_server != null) "--heimdall_rest_server ${cfg.heimdall_rest_server}"} \
-              ${lib.optionalString (cfg.logs_writer_file != null) "--logs_writer_file ${cfg.logs_writer_file}"} \
+              --logs_writer_file ${cfg.logs_writer_file} \
               ${lib.optionalString (cfg.main_chain_gas_limit != null) "--main_chain_gas_limit ${toString cfg.main_chain_gas_limit}"} \
               ${lib.optionalString (cfg.main_chain_max_gas_price != null) "--main_chain_max_gas_price ${toString cfg.main_chain_max_gas_price}"} \
-              ${lib.optionalString (cfg.milestone_poll_interval != null) "--milestone_poll_interval ${cfg.milestone_poll_interval}"} \
+              --milestone_poll_interval ${cfg.milestone_poll_interval} \
               ${lib.optionalString (cfg.no_ack_wait_time != null) "--no_ack_wait_time ${cfg.no_ack_wait_time}"} \
-              ${lib.optionalString (cfg.noack_poll_interval != null) "--noack_poll_interval ${cfg.noack_poll_interval}"} \
-              ${lib.optionalString (cfg.span_poll_interval != null) "--span_poll_interval ${cfg.span_poll_interval}"} \
-              ${lib.optionalString (cfg.syncer_poll_interval != null) "--syncer_poll_interval ${cfg.syncer_poll_interval}"} \
-              ${lib.optionalString (cfg.tendermint_rpc_url != null) "--tendermint_rpc_url ${cfg.tendermint_rpc_url}"} \
+              ${lib.optionalString (cfg.seeds != null) "--seeds ${cfg.seeds}"} \
               ${lib.optionalString cfg.trace "--trace"} \
-              ${lib.optionalString (cfg.seeds != null) "--seeds ${cfg.seeds}"}
+              ${lib.escapeShellArgs cfg.extraArgs}
           '';
           DynamicUser = true;
           Restart = "always";
