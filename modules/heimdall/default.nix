0ba518466935f510c1af5c5505c96ee671d43cc3
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-10-29 14:26:48 -0300

    add seeds parameters

--- modules/heimdall/default.nix
+++ modules/heimdall/default.nix
@@ -49,6 +49,12 @@ let
         description = "Additional arguments for the Heimdall executable.";
       };
 
+      seeds = lib.mkOption {
+        type = lib.types.str;
+        default = "1500161dd491b67fb1ac81868952be49e2509c9f@52.78.36.216:26656,dd4a3f1750af5765266231b9d8ac764599921736@3.36.224.80:26656,8ea4f592ad6cc38d7532aff418d1fb97052463af@34.240.245.39:26656,e772e1fb8c3492a9570a377a5eafdb1dc53cd778@54.194.245.5:26656,6726b826df45ac8e9afb4bdb2469c7771bd797f1@52.209.21.164:26656";
+        description = "List of seeds to connect to.";
+      };
+
       package = lib.mkPackageOption pkgs [ "heimdall-polygon" ] { };
     };
   };
@@ -85,6 +91,7 @@ in {
               --chain ${cfg.chain} \
               --amqp_url ${cfg.amqp_url} \
               --bor_rpc_url ${cfg.bor_rpc_url} \
+              --seeds ${cfg.seeds} \
               --checkpoint_poll_interval ${cfg.checkpoint_poll_interval} \
               ${lib.optionalString cfg.trace "--trace"} \
               ${lib.escapeShellArgs cfg.extraArgs}
