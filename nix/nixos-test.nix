d19a02d95b8832fec905e7adb72fcfac928a26d8
Bruno Nascimento <brunonascimentodev@gmail.com>
2024-11-13 22:39:44 -0300

    add /status on test

--- nix/nixos-test.nix
+++ nix/nixos-test.nix
@@ -16,6 +16,6 @@ pkgs.nixosTest {
     # Ensure the service is started and reachable
     machine.wait_for_unit("polygon-heimdall.service")
     machine.wait_for_open_port(1317)
-    machine.succeed("curl --fail http://127.0.0.1:1317")
+    machine.succeed("curl --fail http://127.0.0.1:1317/status")
   '';
 }
